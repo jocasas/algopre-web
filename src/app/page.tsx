9d51e7210e1c7593a243ac5536863ed93a7168e7
Vrivas99 <vi.rivas@duocuc.cl>
2023-05-31 04:45:14 -0400

    dashboard
    se implemento dashboard para presentacion de avance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-
+import ProbableFallsCards from './components/ProbableFallsCards'
+import HistoricFalls from './components/HistoricFalls'
+import LastFalls from './components/LastFalls'
 
 export default function Home() {
   return (
     <>
-      <h1>aqui los graficos!!</h1>
+      <ProbableFallsCards />
+      <div className='grid md:grid-cols-3 grid-cols-1 gap-4'>
+        <HistoricFalls />
+        <LastFalls />
+      </div>
     </>
-
   )
 }
