67bc03ff333c36da006278db193fcaf2ae7dda52
jocasas <102597441+jocasas@users.noreply.github.com>
2023-06-12 02:55:57 -0400

    tratando de recorrer la bd

--- lib/database/database.ts
+++ lib/database/database.ts
@@ -1,10 +1,11 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "20173",
   database: "test01",
+  port: 3306,
 });
 
 export default connection;
--- src/app/mlPage/page.tsx
+++ src/app/mlPage/page.tsx
@@ -1,23 +1,47 @@
-import React from 'react'
-import { authOptions } from '@/utils/authOptions'
+import React from "react";
+import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-
-
-
-export default async function page() {
-
-    const session = await getServerSession(authOptions)
-    //valida si es que el usuario esta logeado sino se le redirege al login
-    if (!session) {
-        redirect("/")
+import connection from "../../../lib/database/database";
+//--------------------------------------------------------------------------
+/* async function fetchCrabs() {
+  console.log("||| ||| fetchCrabs corriedo ||| |||");
+  connection.query(
+    "SELECT * FROM `crabs` LIMIT 2",
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
     }
+  );
+  }                                                                      */
+//--------------------------------------------------------------------------
+export default async function page() {
+  const fetchData = () => {
+    try {
+      connection.query(
+        "SELECT * FROM `crabs` LIMIT 2",
+        function (err, results, fields) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(results); // results contains rows returned by server
+          }
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchData();
 
-    return (
-        <div className='w-full h-screen flex justify-center items-center'>
-            <div>
-                aquí van los gráficos de ML ༼ つ ◕_◕ ༽つ
-            </div>
-        </div>
-    )
+  const session = await getServerSession(authOptions);
+  //valida si es que el usuario esta logeado sino se le redirege al login
+  if (!session) {
+    redirect("/");
+  }
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <div>aquí van los gráficos de ML ༼ つ ◕_◕ ༽つ</div>
+    </div>
+  );
 }
