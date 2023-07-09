f441fc27c3896ed12ab2cc5fc6668de64bd00f76
jocasas <jo.casas@duocuc.cl>
2023-07-09 19:50:26 -0400

    configuraciones

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
-'use client'
-import { useRef } from 'react'
-import Logo from '../../public/img/logo.png'
-import Image from "next/image"
-import { signIn } from 'next-auth/react'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+"use client";
+import { useRef } from "react";
+import Logo from "../../public/img/logo.png";
+import Image from "next/image";
+import { signIn } from "next-auth/react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const userName = useRef("")
-  const pass = useRef("")
+  const userName = useRef("");
+  const pass = useRef("");
+  const router = useRouter();
   //se valida los datos requeridos por credentials en utils/authOptions.tsx con los datos ingresados en los inputs al momento de hacer click en el boton
-  const onSubmit = async () => {
+  const onSubmit = async (event: any) => {
+    event.preventDefault();
     const result = await signIn("credentials", {
       email: userName.current,
       password: pass.current,
@@ -19,7 +22,7 @@ export default function Home() {
     });
     //si el resultado es un error se muestra un toast con el mensaje de error
     if (result?.error || userName.current === "" || pass.current === "") {
-      toast.error('Credenciales invalidas', {
+      toast.error("Credenciales invalidas", {
         position: "bottom-center",
         autoClose: 2000,
         hideProgressBar: false,
@@ -29,46 +32,76 @@ export default function Home() {
         progress: 0,
         theme: "dark",
       });
-
     } else {
       //si el resultado es correcto se redirecciona a la pagina de mlPage
-      window.location.href = "/mlPage"
+      //window.location.href = "/mlPage";
+      router.push("/mlPage");
     }
-  }
-
+  };
 
   return (
-    <div className='h-screen w-full bg-[url(../../public/img/bg.jpg)]'>
+    <div className="h-screen w-full bg-[url(../../public/img/bg.jpg)]">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <Image src={Logo} alt="logo" className="mx-auto h-16 w-auto"></Image>
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Ingresa a tu cuenta</h2>
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+            Ingresa a tu cuenta
+          </h2>
         </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" method="POST">
             <div>
-              <label htmlFor="input-email-for-credentials-provider" className="block text-sm font-medium leading-6 text-white">Correo</label>
+              <label
+                htmlFor="input-email-for-credentials-provider"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Correo
+              </label>
               <div className="mt-2">
-                <input id="input-email-for-credentials-provider" onChange={(e) => (userName.current = e.target.value)} placeholder="Correo" name="Email" type="email" autoComplete="email" className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6" />
+                <input
+                  id="input-email-for-credentials-provider"
+                  onChange={(e) => (userName.current = e.target.value)}
+                  placeholder="Correo"
+                  name="Email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                />
               </div>
             </div>
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="input-password-for-credentials-provider" className="block text-sm font-medium leading-6 text-white">Contraseña</label>
+                <label
+                  htmlFor="input-password-for-credentials-provider"
+                  className="block text-sm font-medium leading-6 text-white"
+                >
+                  Contraseña
+                </label>
               </div>
               <div className="mt-2">
-                <input id="input-password-for-credentials-provider" onChange={(e) => (pass.current = e.target.value)} placeholder="Contraseña" name="password" type="password" autoComplete="current-password" className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6" />
+                <input
+                  id="input-password-for-credentials-provider"
+                  onChange={(e) => (pass.current = e.target.value)}
+                  placeholder="Contraseña"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                />
               </div>
             </div>
             <div>
-              <button type="submit" onClick={onSubmit} className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Ingresar</button>
+              <button
+                onClick={onSubmit}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Ingresar
+              </button>
             </div>
           </form>
           <ToastContainer />
         </div>
       </div>
     </div>
-
-  )
+  );
 }
-
--- src/app/utils/authOptions.tsx
+++ src/app/utils/authOptions.tsx
@@ -1,53 +1,59 @@
 import { NextAuthOptions } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { signOut } from "next-auth/react";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        Credentials({
-            name: "Credentials",
-            credentials: {
-                //credenciales necesarias para logear
-                email: { label: "Correo", type: "text" },
-                password: { label: "Contraseña", type: "password" },
-            },
-            authorize(credentials, req) {
-                //aqui deberia ser llamado desde la base de datos
-                //validacion de usuario
-                const users = [
-                    {
-                        id: '1',
-                        name: 'vi.rivas',
-                        email: 'vi.rivas@duocuc.cl',
-                        password: 'vi.rivas',
-                    }, {
-                        id: '2',
-                        name: 'admin',
-                        email: 'admin@admin.cl',
-                        password: 'admin'
-                    }, {
-                        id: '3',
-                        name: 'jo.casas',
-                        email: 'jo.casas@duocuc.cl',
-                        password: 'jo.casas',
-                    }
-                ];
-                //recorre array de usuarios y valida si es que el usuario y contraseña ingresados son iguales a los de la base de datos
-                for (const user of users) {
-                    if (credentials?.email === user.email && credentials?.password === user.password) {
-                        return {
-                            id: user.id,
-                            email: user.email,
-                            name: user.name,
-                        };
-                    }
-                }
-                return null;
-            }
-        })
-    ],
-    // pagina principal donde se realizara el logIn 
-    pages: {
-        signIn: '/'
-    },
-
-}
+  providers: [
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        //credenciales necesarias para logear
+        email: { label: "Correo", type: "text" },
+        password: { label: "Contraseña", type: "password" },
+      },
+      authorize(credentials, req) {
+        //aqui deberia ser llamado desde la base de datos
+        //validacion de usuario
+        const users = [
+          {
+            id: "1",
+            name: "vi.rivas",
+            email: "vi.rivas@duocuc.cl",
+            password: "vi.rivas",
+          },
+          {
+            id: "2",
+            name: "admin",
+            email: "admin@admin.cl",
+            password: "admin",
+          },
+          {
+            id: "3",
+            name: "jo.casas",
+            email: "jo.casas@duocuc.cl",
+            password: "jo.casas",
+          },
+        ];
+        //recorre array de usuarios y valida si es que el usuario y contraseña ingresados son iguales a los de la base de datos
+        for (const user of users) {
+          if (
+            credentials?.email === user.email &&
+            credentials?.password === user.password
+          ) {
+            return {
+              id: user.id,
+              email: user.email,
+              name: user.name,
+            };
+          }
+        }
+        return null;
+      },
+    }),
+  ],
+  // pagina principal donde se realizara el logIn
+  pages: {
+    signIn: "/",
+    signOut: "/",
+  },
+};
